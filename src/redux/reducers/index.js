56d84289452f55eaf15253f2a8afcc91a0401b01
Abdessamed Seddiki <102583087+AbdessamedSed@users.noreply.github.com>
2023-06-24 23:58:49 +0100

    del

--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import counterReducer from "./counterReducer";
-import userRducer from "./userReducer";
-
-export default combineReducers({
-    counter: counterReducer,
-    user: userRducer
-})
--- src/redux/reducers/userReducer.js
+++
@@ -1,5 +0,0 @@
-const userRducer = (state = {}, action) => {
-    return state;
-}
-
-export default userRducer;
